eba304d47f999e39ab41e08ec45538e05c420c88
Nader Morshed <morshed.nader@gmail.com>
2015-02-02 18:13:42 -0500

    Added more seralizers

--- proteomics/serializers.py
+++ proteomics/serializers.py
@@ -4,12 +4,32 @@ from proteomics import models
 from rest_framework import serializers
 
 # Serializers define the API representation.
+class PeptideMethodSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.PeptideMethod
+        fields = ()
+
+class MSMethodSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.MSMethod
+        fields = (
+            "protocol_type",
+            "enrichment",
+            "validated_spectra",
+            "cptac_id",
+        )
+
+class PeptideDataSetSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = models.PeptideDataSet
+        fields = ("method", "description",)
+
 class PeptideSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Peptide
-        fields = ()
+        fields = ("data_set", "sequence", "protein", "phosphosite_plus_id",)
 
 class ProteinSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = models.Protein
-        fields = ()
+        fields = ("uniprot_id",)
