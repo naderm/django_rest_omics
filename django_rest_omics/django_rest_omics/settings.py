12114da6e795d49f2a8dce653f86cdee38f8ca2d
Nader Morshed <morshed.nader@gmail.com>
2015-02-02 12:46:31 -0500

    Added REST settings

--- django_rest_omics/django_rest_omics/settings.py
+++ django_rest_omics/django_rest_omics/settings.py
@@ -64,6 +64,15 @@ DATABASES = {
     }
 }
 
+# REST Framework
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
